b2b8ab24a891976c4ee34a5fe757ee436bc22b03
leferrad <ljferrado@gmail.com>
2018-07-08 19:45:00 -0300

    Fix on test_tesseract

--- test/test_tesseract.jl
+++ test/test_tesseract.jl
@@ -7,10 +7,8 @@ module TestOCReract
 
 path_to_test_img = "$(pwd())/test/files/testocr.png"
 
-expected_text = "This is a lot of 12 point text to test the\nocr code and see if it works on all types\nof file format.\n\nThe quick brown dog jumped over the\nlazy fox. The quick brown dog jumped\nover the lazy fox. The quick brown dog\njumped over the lazy fox. The quick\nbrown dog jumped over the lazy fox.\n\f"
-
 function test()
-    expected_text = "This is a lot of 12 point text to test the\nocr code and see if it works on all types\nof file format.\n\nThe quick brown dog jumped over the\nlazy fox. The quick brown dog jumped\nover the lazy fox. The quick brown dog\njumped over the lazy fox. The quick\nbrown dog jumped over the lazy fox.\n\f"
+    expected_text = "This is a lot of 12 point text to test the\nocr code and see if it works on all types\nof file format.\n\nThe quick brown dog jumped over the\nlazy fox. The quick brown dog jumped\nover the lazy fox. The quick brown dog\njumped over the lazy fox. The quick\nbrown dog jumped over the lazy fox."
     
     run_tesseract(path_to_test_img, "/tmp/res")
         
@@ -20,7 +18,7 @@ function test()
         result_txt = readstring(f)
     end
 
-    Base.Test.@test expected_text == result_txt
+    Base.Test.@test strip(expected_text) == strip(result_txt)
 
 end
 
