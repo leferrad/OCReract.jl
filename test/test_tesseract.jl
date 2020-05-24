91a810c44085a79ab584325769bb89f652e16958
Leandro Ferrado <ljferrado@gmail.com>
2020-05-24 15:33:40 -0300

    Updated tests

--- test/test_tesseract.jl
+++ test/test_tesseract.jl
@@ -3,35 +3,43 @@ using OCReract
 using Test
 
 pkg_path = abspath(joinpath(dirname(pathof(OCReract)), ".."))
-path_to_test_img = "$(pkg_path)/test/files/testocr.png"
 
-expected_text_test = "This is a lot of 12 point text to test the\n"*
-                     "ocr code and see if it works on all types\n"*
-                     "of file format.\n\n"*
-                     "The quick brown dog jumped over the\n"*
-                     "lazy fox. The quick brown dog jumped\n"*
-                     "over the lazy fox. The quick brown dog\n"*
-                     "jumped over the lazy fox. The quick\n"*
-                     "brown dog jumped over the lazy fox."
+TEST_ITEMS = Dict(
+    "simple" => Dict(
+        "path" => "$(pkg_path)/test/files/testocr.png",
+        "text" => "This is a lot of 12 point text to test the\n"*
+                  "ocr code and see if it works on all types\n"*
+                  "of file format.\n\n"*
+                  "The quick brown dog jumped over the\n"*
+                  "lazy fox. The quick brown dog jumped\n"*
+                  "over the lazy fox. The quick brown dog\n"*
+                  "jumped over the lazy fox. The quick\n"*
+                  "brown dog jumped over the lazy fox."
+    ),
+    "noisy" => Dict(
+        "path" => "$(pkg_path)/test/files/noisy.png",
+        "text" => "Noisy image\nto test\nOCReract.jl"
+    )
+)
 
 function test_run_tesseract()
     tmp_path = tempname()
     mkdir(tmp_path)
-    path_to_res = tmp_path*"/res"
-    res = run_tesseract(path_to_test_img, path_to_res)
+    path_to_res = tmp_path*"/res.txt"
+    res = run_tesseract(TEST_ITEMS["simple"]["path"], path_to_res)
     @test res == true
 
     result_txt = ""
-    open(path_to_res*".txt", "r") do f
+    open(path_to_res, "r") do f
         result_txt = read(f, String)
     end
-    @test strip(expected_text_test) == strip(result_txt)
+    @test strip(result_txt) == strip(TEST_ITEMS["simple"]["text"])
 
 end
 
 function test_run_and_get_output()
-    result_txt = run_and_get_output(Images.load(path_to_test_img))
-    @test strip(expected_text_test) == strip(result_txt)
+    result_txt = run_tesseract(Images.load(TEST_ITEMS["simple"]["path"]))
+    @test strip(TEST_ITEMS["simple"]["text"]) == strip(result_txt)
 end
 
 function test_path_exist()
@@ -44,12 +52,12 @@ function test_bad_arguments()
     # Bad Language
     tmp_path = tempname()
     mkdir(tmp_path)
-    path_to_res = tmp_path*"/res"
-    res = run_tesseract(path_to_test_img, path_to_res, lang="not-existing-lang")
+    path_to_res = tmp_path*"/res.txt"
+    res = run_tesseract(TEST_ITEMS["simple"]["path"], path_to_res, lang="not-existing-lang")
     @test res == false
 
     # Despite bad parameters oem and psm, this will work due to default changes
-    res = run_tesseract(path_to_test_img, path_to_res, oem=100, psm=100)
+    res = run_tesseract(TEST_ITEMS["simple"]["path"], path_to_res, oem=100, psm=100)
     @test res == true
 end
 
